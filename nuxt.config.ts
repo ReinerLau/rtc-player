01ca99b7a2ab4af447f1bea2f40560cd98771a8f
ReinerLau <lk850593913@gmail.com>
2024-05-30 17:41:05 +0800

    build: primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/ui", "@nuxtjs/tailwindcss", "nuxt-primevue"],
   imports: {
     autoImport: false,
   },
+  css: ["primevue/resources/themes/aura-dark-green/theme.css"],
 });
