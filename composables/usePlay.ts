c26336d0a469bd1d688221ace1763276a90ff976
ReinerLau <lk850593913@gmail.com>
2024-06-03 16:11:58 +0800

    feat: close all video before pull

--- composables/usePlay.ts
+++ composables/usePlay.ts
@@ -2,16 +2,27 @@ import type { PlayAllParams, Video } from "~/types";
 import { getIndex, SrsRtcPlayerAsync } from "~/utils";
 
 export const usePlay = (videoList: Video[]) => {
+  const srsList: SrsRtcPlayerAsync[] = [];
+
   const playAll = ({ page, videoEls }: PlayAllParams) => {
+    closeAll();
     const total = videoEls.length;
     videoEls.forEach((videoEl, index) => {
       const srs = new SrsRtcPlayerAsync();
+      srsList.push(srs);
       videoEl.srcObject = srs.stream;
       const url = videoList[getIndex(page, total, index)]?.url;
       if (url) {
         srs.play(url);
       }
     });
+  };
+
+  const closeAll = () => {
+    srsList.forEach((srs) => {
+      srs.close();
+    });
+    srsList.length = 0;
   };
 
   return {
--- tests/usePlay.test.ts
+++ tests/usePlay.test.ts
@@ -4,26 +4,36 @@ import type { Video } from "~/types";
 
 describe("播放视频", () => {
   const play = vi.hoisted(() => vi.fn());
+  const close = vi.hoisted(() => vi.fn());
   vi.mock("~/utils", async (importOriginal) => {
     const mod = await importOriginal<typeof import("~/utils")>();
     return {
       ...mod,
       SrsRtcPlayerAsync: vi.fn(() => ({
         play,
+        close,
       })),
     };
   });
+  const videoList: Video[] = [
+    {
+      id: 1,
+      name: "test",
+      url: "test",
+    },
+    {
+      id: 2,
+      name: "test1",
+      url: "test1",
+    },
+  ];
+  const page = 1;
+  const videoEls = [
+    document.createElement("video"),
+    document.createElement("video"),
+  ];
 
   it("happy path", () => {
-    const videoList: Video[] = [
-      {
-        id: 1,
-        name: "test",
-        url: "test",
-      },
-    ];
-    const page = 1;
-    const videoEls = [document.createElement("video")];
     const { playAll } = usePlay(videoList);
 
     playAll({
@@ -32,5 +42,28 @@ describe("播放视频", () => {
     });
 
     expect(play).toHaveBeenCalledWith(videoList[0].url);
+    expect(play).toHaveBeenCalledWith(videoList[1].url);
+  });
+
+  it("每次拉流前先关闭已有视频流", () => {
+    const videoList: Video[] = [
+      {
+        id: 1,
+        name: "test",
+        url: "test",
+      },
+    ];
+    const { playAll } = usePlay(videoList);
+
+    playAll({
+      page,
+      videoEls,
+    });
+    playAll({
+      page,
+      videoEls,
+    });
+
+    expect(close).toBeCalledTimes(2);
   });
 });
